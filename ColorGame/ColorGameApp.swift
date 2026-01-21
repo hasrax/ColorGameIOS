1822db07b95a82ca0350bb5f29b554c25ca81fca
hasrax <hasragunawardena093@gmail.com>
2026-01-21 11:15:39 +0530

    Initial Commit

---
+++ ColorGame/ColorGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGameApp.swift
+//  ColorGame
+//
+//  Created by COBSCCOMP24.2P-023 on 2026-01-21.
+//
+
+import SwiftUI
+
+@main
+struct ColorGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorGame
+//
+//  Created by COBSCCOMP24.2P-023 on 2026-01-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
